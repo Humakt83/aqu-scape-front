ce46850fb9af1ff4f9ebe3f071a0b86c81c42621
Humakt83 <urlact@yahoo.com>
2016-08-28 21:15:44 +0300

    Styling...

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'canvas',
+    selector: 'canvas-of-objects',
     templateUrl: 'app/canvas/canvas.html',
     styleUrls: ['app/canvas/canvas.css']
 })
