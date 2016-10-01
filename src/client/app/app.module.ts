3d877da6545f2c4ce12c2fd2820c3ebab7d65553
Humakt83 <urlact@yahoo.com>
2016-10-01 23:55:41 +0300

    rename palette and add paper to project

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule  } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { MainComponent }   from './main/main.component';
-import { CanvasComponent } from './canvas/canvas.component';
+import { PaletteComponent } from './palette/palette.component';
 import { DrawingAreaComponent } from './drawing-area/drawing-area.component';
+import { PlantService } from './shared/index';
 
 @NgModule({
-    declarations: [MainComponent, CanvasComponent, DrawingAreaComponent],
-    imports:      [BrowserModule, HttpModule],
-    bootstrap:    [MainComponent],
+    declarations: [MainComponent, PaletteComponent, DrawingAreaComponent],
+    imports: [BrowserModule, HttpModule],
+    bootstrap: [MainComponent],
+    providers: [HttpModule, PlantService]
 })
 export class AppModule {}
--- src/client/app/main/main.component.ts
+++ src/client/app/main/main.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { CanvasComponent } from '../canvas/canvas.component';
-import { DrawingAreaComponent } from '../drawing-area/drawing-area.component';
-import { PlantService } from '../shared/index';
 
 @Component({
     selector: 'aqu',
     templateUrl: 'app/main/main.html',
-    providers: [ HttpModule, PlantService ],
-    directives: [ CanvasComponent, DrawingAreaComponent ]
 })
 export class MainComponent {
 }
